2324f8ac2b37cb5ca4b4823ebed1c0bf6d5283d6
unknown <matteo.gilardoni@User-PC.sol.kurtosys.com>
2015-01-25 12:28:18 +0000

    completed lightbox and tooltip styles

--- app/src/absence-input/directives/absence-input-directive.js
+++ app/src/absence-input/directives/absence-input-directive.js
@@ -7,5 +7,28 @@ angular
 			  retrict: 'E'
 			, replace: true
 			, templateUrl: '/src/absence-input/views/absence-input.html'
+			, scope : { name: '=name' }
+			, link : function (scope, element, attrs) {
+				var name = attrs.name;
+
+				element.eq(0).addClass(name);
+				
+				var elm$LabelsList = element.find('label');
+				var elm$InputsList = element.find('input');
+
+				for (var i = elm$LabelsList.length - 1; i >= 0; i--) {
+					// map elements
+					var elm$Label = elm$LabelsList.eq(i);
+					var elm$Input = elm$InputsList.eq(i);
+
+					// create string
+					var string = elm$Label.attr('for');
+					string = name + '-' + string;
+					
+					// replace for and id
+					elm$Label.attr('for', string);
+					elm$Input.attr('id', string);
+				};
+			}
 		};
 	});
